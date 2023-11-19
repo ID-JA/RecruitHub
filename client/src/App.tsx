672a06fa2c71887f555272a84a66d37ccf02f49d
ID-JA <idaissa93@gmail.com>
2023-11-19 14:50:39 +0100

    Add react-query-devtools and eslint-plugin-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import { Container, Grid, Skeleton, Title } from '@mantine/core';
+import { useGetHelloWorld } from './api/hello-world';
+
 const child = <Skeleton height={140} radius="md" animate />;
 
 function App() {
+  const {data, isLoading} = useGetHelloWorld()
   return (
     <Container my="md">
-      <Title order={1} >Recruit Hub 🚀</Title>
+      <Title order={1} >{isLoading ? "Loading..." : data}</Title>
       <Grid>
         <Grid.Col span={{ base: 12, xs: 4 }}>{child}</Grid.Col>
         <Grid.Col span={{ base: 12, xs: 8 }}>{child}</Grid.Col>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,22 @@
+import "@mantine/core/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { MantineProvider, createTheme } from "@mantine/core";
-import "@mantine/core/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const theme = createTheme({
-  /** Put your mantine theme override here */
-});
+const theme = createTheme({});
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MantineProvider theme={theme}>
-      <App />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme}>
+      <ReactQueryDevtools initialIsOpen={false} />
+        <App />
+      </MantineProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
