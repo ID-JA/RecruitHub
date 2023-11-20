ba8e694488409764da837ca30266082168cf8da4
ID-JA <idaissa93@gmail.com>
2023-11-20 08:18:29 +0100

    Feat: setup application routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,71 @@
-import { Container, Grid, Skeleton, Title } from '@mantine/core';
+import { Box, Button, Container, Flex, Grid, Group, Skeleton, Text, Title } from '@mantine/core';
 import { useGetHelloWorld } from './api/hello-world';
+
+import { Outlet, Link } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+// Create a root route
 
 const child = <Skeleton height={140} radius='md' animate />;
 
 function App() {
   const { data, isLoading } = useGetHelloWorld();
   return (
-    <Container my='md'>
-      <Title order={1}>{isLoading ? 'Loading...' : data}</Title>
-      <Grid>
-        <Grid.Col span={{ base: 12, xs: 4 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 8 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 8 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 4 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 3 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 3 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 6 }}>{child}</Grid.Col>
-      </Grid>
-    </Container>
+    <>
+      <Box p='sm'>
+        <Group align='center' justify='space-between'>
+          <Flex gap='md'>
+            <Link to='/'>
+              <Text>Home</Text>
+            </Link>
+            <Link to='/about'>
+              <Text>About</Text>
+            </Link>
+          </Flex>
+          <Flex gap='md'>
+            <Link to='/'>
+              <Button>Post a job</Button>
+            </Link>
+            <Link to='/about'>
+              <Button variant='outline'>Sign in</Button>
+            </Link>
+          </Flex>
+        </Group>
+      </Box>
+      <Container my='md'>
+        <Outlet />
+        <Title order={1}>{isLoading ? 'Loading...' : data}</Title>
+        <Grid>
+          <Grid.Col span={{ base: 12, xs: 4 }}>{child}</Grid.Col>
+          <Grid.Col span={{ base: 12, xs: 8 }}>{child}</Grid.Col>
+          <Grid.Col span={{ base: 12, xs: 8 }}>{child}</Grid.Col>
+          <Grid.Col span={{ base: 12, xs: 4 }}>{child}</Grid.Col>
+          <Grid.Col span={{ base: 12, xs: 3 }}>{child}</Grid.Col>
+          <Grid.Col span={{ base: 12, xs: 3 }}>{child}</Grid.Col>
+          <Grid.Col span={{ base: 12, xs: 6 }}>{child}</Grid.Col>
+        </Grid>
+        <TanStackRouterDevtools />
+      </Container>
+    </>
   );
 }
+
+export function Index() {
+  return (
+    <div>
+      <h3>Welcome Home!</h3>
+    </div>
+  );
+}
+
+export function About() {
+  return <div>Hello from About!</div>;
+}
+
+// Create the route tree using your routes
+
+// Create the router using your route tree
+
+// Register your router for maximum type safety
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,21 +1,22 @@
-import "@mantine/core/styles.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { MantineProvider, createTheme } from "@mantine/core";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import '@mantine/core/styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MantineProvider, createTheme } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RouterProvider } from '@tanstack/react-router';
+import { router } from './routes/index.ts';
 
 const theme = createTheme({});
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <MantineProvider theme={theme}>
-      <ReactQueryDevtools initialIsOpen={false} />
-        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+        <RouterProvider router={router} />
       </MantineProvider>
     </QueryClientProvider>
   </React.StrictMode>
