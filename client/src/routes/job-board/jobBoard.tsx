6085df358623165fda7f31155dc95211030239ed
iman <iman@gmail.com>
2023-12-04 12:17:05 +0100

    update

--- client/src/routes/job-board/jobBoard.tsx
+++ client/src/routes/job-board/jobBoard.tsx
@@ -1,42 +1,17 @@
 import { Route } from '@tanstack/react-router';
 import { defaultLayoutRoute } from '../../layouts/default-layout';
-import { Container, Group, Burger, Box, Anchor } from '@mantine/core';
+import { Container, Burger, Tabs } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { TextInput, ActionIcon, useMantineTheme, rem } from '@mantine/core';
 import { IconSearch, IconArrowRight } from '@tabler/icons-react';
 import classes from './DoubleHeader.module.css';
-import { useState } from 'react';
 
 export function JobBoard() {
   const theme = useMantineTheme();
   const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(0);
-
-  const mainLinks = [
-    { link: '#', label: 'For You' },
-    { link: '#', label: 'Applied Jobs' },
-    { link: '#', label: 'Saved Jobs' }
-  ];
-
-  const mainItems = mainLinks.map((item, index) => (
-    <Anchor<'a'>
-      href={item.link}
-      key={item.label}
-      className={`${classes.mainLink} ${index === active ? classes.active : ''}`}
-      data-active={index === active || undefined}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(index);
-      }}
-    >
-      {item.label}
-    </Anchor>
-  ));
 
   return (
     <Container className={classes.jobBoardContainer}>
-      {/* Add the InputWithButton component at the top center */}
-
       <TextInput
         radius='xl'
         size='xs'
@@ -51,14 +26,17 @@ export function JobBoard() {
         }
       />
 
-      {/* DoubleHeader (Navbar) */}
+      {/* Add the Tabs component here */}
+      <Tabs defaultValue='first'>
+        <Tabs.List justify='center'>
+          <Tabs.Tab value='first'>First tab</Tabs.Tab>
+          <Tabs.Tab value='second'>Second tab</Tabs.Tab>
+          <Tabs.Tab value='third'>Third tab</Tabs.Tab>
+        </Tabs.List>
+      </Tabs>
+
       <header className={classes.header}>
         <Container className={classes.inner}>
-          <Box className={classes.links} visibleFrom='sm'>
-            <Group gap={0} justify='center' className={classes.mainLinks}>
-              {mainItems}
-            </Group>
-          </Box>
           <Burger
             opened={opened}
             onClick={toggle}
@@ -71,21 +49,6 @@ export function JobBoard() {
     </Container>
   );
 }
-
-// ForYouContent.tsx
-/*export function ForYouContent() {
-  return <div>Content for For You tab</div>;
-}
-
-// AppliedJobsContent.tsx
-export function AppliedJobsContent() {
-  return <div>Content for Applied Jobs tab</div>;
-}
-
-// SavedJobsContent.tsx
-export function SavedJobsContent() {
-  return <div>Content for Saved Jobs tab</div>;
-}*/
 
 export const jobBoardRoute = new Route({
   path: 'jobBoard',
