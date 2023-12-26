d01a50dc8c0b9abd7b640e74b8c2dd0221535a1e
younes talibi <younessetalibi11@gmail.com>
2023-12-26 12:06:42 +0100

    setup notifications in the client

---
+++ client/src/routes/Notifications/Notifications.ts
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import Pusher from 'pusher-js';
+import Echo from 'laravel-echo';
+
+export const Notifications = () => {
+  window.Pusher = Pusher;
+  Echo.logToConsole = true;
+  window.Echo = new Echo({
+    broadcaster: '///',
+    key: '///',
+    cluster: '///',
+    encrypted: '///'
+  });
+  useEffect(() => {
+    const channel = window.Echo.channel('App.Models.User.1');
+    channel.listen('.Notifications', function (data: object) {
+      console.log(JSON.stringify(data));
+      //formdata of notifications that you will recieve is {title,body} make state for it.
+      //App.Models.User.1 !! don't forget to replace the number 1
+      //with the actual user id (the current user id)
+      //don't change anything else .
+      //copy this code and put it in the compocnent where notification lives
+    });
+    return () => {
+      window.Echo.leave('App.Models.User.1');
+    };
+  }, []);
+
+  return; ///;
+};
