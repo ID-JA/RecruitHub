f1e6d69a967162ba533509f1322acbba0928f3e4
ID-JA <idaissa93@gmail.com>
2023-12-30 13:13:03 +0100

    FEAT: add job's actions

--- client/src/components/jobs/job-card.tsx
+++ client/src/components/jobs/job-card.tsx
@@ -1,9 +1,20 @@
-import { ActionIcon, Avatar, Badge, Group, Paper, Popover, Text, Title } from '@mantine/core';
-import { type JobProps } from '../../types';
+import {
+  ActionIcon,
+  Avatar,
+  Badge,
+  Group,
+  Menu,
+  Paper,
+  Popover,
+  Text,
+  Title,
+  rem
+} from '@mantine/core';
 import { timeAgo } from '../../utils';
-import { IconDots } from '@tabler/icons-react';
+import { IconArchive, IconDots, IconTrash, IconUpload } from '@tabler/icons-react';
 import { useDisclosure } from '@mantine/hooks';
 import { JobData } from './jobs-container';
+import { IconEdit } from '@tabler/icons-react';
 
 function JobCard({ props }: { props: JobData }) {
   const [opened, { open, close }] = useDisclosure();
@@ -73,16 +84,46 @@ function JobCard({ props }: { props: JobData }) {
           </Group>
         </div>
         <Group>
-          <Badge color={props.status === 'active' ? 'green' : 'orange'}>{props.status}</Badge>
-          <ActionIcon variant='subtle' color='gray.7' aria-label='Exit full screen'>
-            <IconDots style={{ width: '70%', height: '70%' }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon variant='subtle' color='gray.7' aria-label='Exit full screen'>
-            <IconDots style={{ width: '70%', height: '70%' }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon variant='subtle' color='gray.7' aria-label='Exit full screen'>
-            <IconDots style={{ width: '70%', height: '70%' }} stroke={1.5} />
-          </ActionIcon>
+          <Badge variant='light' color={props.status === 'active' ? 'green' : 'orange'}>
+            {props.status}
+          </Badge>
+          <Badge variant='light' color='gray'>
+            1000
+            <span style={{ marginLeft: '5px' }}>Candidates</span>
+          </Badge>
+          <Menu shadow='md' width={200}>
+            <Menu.Target>
+              <ActionIcon component='div' variant='subtle' color='gray.7' aria-label='more actions'>
+                <IconDots style={{ width: '70%', height: '70%' }} stroke={1.5} />
+              </ActionIcon>
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              {props.status === 'pending' && (
+                <Menu.Item
+                  key='post-new-job'
+                  fw='bold'
+                  color='green'
+                  leftSection={<IconUpload style={{ width: rem(14), height: rem(14) }} />}
+                >
+                  Post
+                </Menu.Item>
+              )}
+              <Menu.Item leftSection={<IconEdit size={18} />}>Edit</Menu.Item>
+              {props.status === 'active' && (
+                <Menu.Item key='close-offer' leftSection={<IconArchive size={18} />}>
+                  Close
+                </Menu.Item>
+              )}
+
+              <Menu.Item
+                color='red'
+                leftSection={<IconTrash style={{ width: rem(14), height: rem(14) }} />}
+              >
+                Delete job
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
         </Group>
       </Group>
     </Paper>
